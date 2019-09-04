478d71cbbd8b5fd00c7c59785e42f145a9ac07ad
Avni Patel Thompson <avnipatelthompson@Avnis-iMac.local>
2019-09-04 09:59:27 -0700

    server up and running

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const server = express();
 
-server.get("/", (res, req) => {
+server.get("/", (req, res) => {
   res.status(200).json({ api: "up" });
 });
 
--- index.js
+++ index.js
@@ -2,4 +2,6 @@ const server = require("./api/server.js");
 
 const port = 8000;
 
-server.lister(port, () => console.log("Running here)"));
+server.listen(port, () => {
+  console.log("Running here");
+});
